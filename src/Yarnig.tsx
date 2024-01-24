cce896d2453a48a7298252b6c1e87b5cf62dcb72
Cheong <xxlsxx1955@gmail.com>
2024-01-24 11:26:01 +0800

    Include VirtualizedList
    This commit include a VirtualizedList and include some default props
    within.

--- src/Yarnig.tsx
+++ src/Yarnig.tsx
@@ -1,13 +1,34 @@
-import {ImageURISource, Modal, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  ImageURISource,
+  Modal,
+  StyleSheet,
+  View,
+  VirtualizedList,
+} from 'react-native';
 
 type Props = {
   images: ImageURISource[];
+  index: number;
 };
 
-export default function Yarnig({images}: Props) {
+export default function Yarnig({images, index = 0}: Props) {
   return (
     <Modal animationType="fade" hardwareAccelerated>
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <VirtualizedList
+          data={images}
+          horizontal
+          pagingEnabled
+          windowSize={2}
+          initialNumToRender={1}
+          maxToRenderPerBatch={1}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          initialScrollIndex={index}
+          renderItem={() => <Image />}
+        />
+      </View>
     </Modal>
   );
 }
