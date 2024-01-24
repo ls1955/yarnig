e151cd46b20d585f70336c4c82f57483f91623ab
Cheong <xxlsxx1955@gmail.com>
2024-01-24 09:14:03 +0800

    Include Yarnig

--- App.jsx
+++ App.jsx
@@ -1,5 +1,13 @@
+import {Text} from 'react-native';
+
 import Yarnig from './src/Yarnig';
 
+import {useFetchURIs} from './src/hooks/useFetchURIs';
+
 export default function App() {
+  const {uris} = useFetchURIs({folder: ''});
+
+  if (!uris) return <Text>Loading, please wait...</Text>;
+
   return <Yarnig />;
 }
